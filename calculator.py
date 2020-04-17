40429a999123ce229380e2f7f150f0cc176d438c
akash2501 <akash.patil18@vit.edu>
2020-04-17 20:48:37 +0530

    My first basic Calculator

--- calculator.py
+++ calculator.py
@@ -9,14 +9,17 @@ print("5. Percentage")
 print("6. Modulus")
 print("7. Exit")
 c=0
+def add(x,y):
+    c = a +b
+    print("Addition is",c)
+
 while True:
     op = input("what you want to do >>")
 
     if op == "1":
         a = float(input("enter first number>"))
         b = float(input("Enter second number>"))
-        c = a + b
-        print("Addtion is",c)
+        add(a,b)
     elif op == "2":
         a = float(input("Enter First number>"))
         b = float(input("Enter Second number>"))
